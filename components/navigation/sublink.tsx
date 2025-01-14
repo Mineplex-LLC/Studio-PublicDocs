a06509e7df72003e59bd7780e000fe7add8db4a4
William <20137336+WillQizza@users.noreply.github.com>
2025-01-14 04:16:09 -0700

    feature: change it so that sublinks only open for depth 1 (#7)
    * Change it so that sublinks only open for a depth of 1
    
    * Strict equality check

--- components/navigation/sublink.tsx
+++ components/navigation/sublink.tsx
@@ -22,9 +22,10 @@ function isRoute(item: Paths): item is Extract<Paths, { title: string; href: str
 
 export default function SubLink(props: Paths & { level: number; isSheet: boolean }) {
   const path = usePathname();
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(props.level === 0);
 
   useEffect(() => {
+    // Expand the SubLink if it is a parent of the current SubLink page the browser is on.
     if (isRoute(props) && props.href && path !== props.href && path.includes(props.href)) {
       setIsOpen(true);
     }
